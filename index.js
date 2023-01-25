cdb2c381a48d4eecb2c77a85e2c7cb2c0e74551e
Jessika Adams <120666250+jessbecoding@users.noreply.github.com>
2023-01-25 13:02:16 -0700

    Working on Sessions
    Also moved create and login to respective author and reader routes

--- index.js
+++ index.js
@@ -4,8 +4,23 @@ const PORT = 3027;
 const authRoutes = require("./routes/authRoutes");
 const authorRoutes = require("./routes/authorRoutes");
 const readerRoutes = require("./routes/readerRoutes");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
 
+// MIDDLEWARE //
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false,
+      maxAge: 2592000000,
+    },
+  })
+);
 
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
@@ -13,6 +28,7 @@ app.set("view engine", "ejs");
 app.use("/auth", authRoutes);
 app.use("/author", authorRoutes);
 app.use("/reader", readerRoutes);
+// MIDDLEWARE //
 
 app.get("/", (req, res) => {
   res.send("Landing Page");
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
 const router = express.Router();
 const { Authors } = require("../sequelize/models");
-const { application } = require("express");
 
+// MIDDLEWARE //
 router.use(express.json());
+const bodyParser = require("body-parser");
+
+router.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+router.use(bodyParser.json());
+router.use(cookieParser());
+router.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false,
+      maxAge: 2592000000,
+    },
+  })
+);
+// MIDDLEWARE //
 
 router.post("/create_author", (req, res) => {
   const { email, password, firstName, lastName, funFact } = req.body;
@@ -35,10 +58,10 @@ router.post("/login_author", async (req, res) => {
       return;
     }
     if (!result) {
-      res.status(401).send("Your password does not match.");
+      res.status(401).send("Your email or password does not match.");
       return;
     }
-    res.status(200).send("Email and Password Match");
+    res.status(200).send("Login Successful!");
   });
 });
 
