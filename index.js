d19d7381521decb1f39d9547f1349015169eace6
Jessika Adams <120666250+jessbecoding@users.noreply.github.com>
2023-01-25 13:36:56 -0700

    Author & Reader Sessions
    Need front end to check if working

--- index.js
+++ index.js
@@ -4,23 +4,9 @@ const PORT = 3027;
 const authRoutes = require("./routes/authRoutes");
 const authorRoutes = require("./routes/authorRoutes");
 const readerRoutes = require("./routes/readerRoutes");
-const session = require("express-session");
-const cookieParser = require("cookie-parser");
 
 // MIDDLEWARE //
 app.use(express.json());
-app.use(cookieParser());
-app.use(
-  session({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      secure: false,
-      maxAge: 2592000000,
-    },
-  })
-);
 
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
@@ -31,7 +17,7 @@ app.use("/reader", readerRoutes);
 // MIDDLEWARE //
 
 app.get("/", (req, res) => {
-	res.render("pages/index");
+  res.send("Landing Page");
 });
 
 app.listen(PORT, console.log(`Listening on port ${PORT}`));
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,8 +14,11 @@ router.use(
     extended: true,
   })
 );
+
 router.use(bodyParser.json());
+
 router.use(cookieParser());
+
 router.use(
   session({
     secret: "secret",
@@ -28,77 +31,5 @@ router.use(
   })
 );
 // MIDDLEWARE //
-
-router.post("/create_author", (req, res) => {
-  const { email, password, firstName, lastName, funFact } = req.body;
-  bcrypt.hash(password, 10, async (err, hash) => {
-    const author = await Authors.create({
-      email: email,
-      password: hash,
-      firstName: firstName,
-      lastName: lastName,
-      funFact: funFact,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
-    res.send(author);
-  });
-});
-
-router.post("/login_author", async (req, res) => {
-  const { email, password } = req.body;
-  const author = await Authors.findOne({
-    where: {
-      email: email,
-    },
-  });
-  bcrypt.compare(password, author.password, (err, result) => {
-    if (err) {
-      res.send(err);
-      return;
-    }
-    if (!result) {
-      res.status(401).send("Your email or password does not match.");
-      return;
-    }
-    res.status(200).send("Login Successful!");
-  });
-});
-
-router.post("/create_reader", (req, res) => {
-  const { email, password, firstName, lastName, funFact } = req.body;
-  bcrypt.hash(password, 10, async (err, hash) => {
-    const reader = await Readers.create({
-      email: email,
-      password: hash,
-      firstName: firstName,
-      lastName: lastName,
-      funFact: funFact,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
-    res.send(reader);
-  });
-});
-
-router.post("/login_reader", async (req, res) => {
-  const { email, password } = req.body;
-  const author = await Reader.findOne({
-    where: {
-      email: email,
-    },
-  });
-  bcrypt.compare(password, reader.password, (err, result) => {
-    if (err) {
-      res.send(err);
-      return;
-    }
-    if (!result) {
-      res.status(401).send("Your password does not match.");
-      return;
-    }
-    res.status(200).send("Email and Password Match");
-  });
-});
 
 module.exports = router;
