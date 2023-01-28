e271a1b8a3cffc3b9eadf0171350bb37c1c3aadf
jssklynn <jessikalynnadams@gmail.com>
2023-01-28 09:50:30 -0700

    updating reader routes to authenticate

--- routes/readerRoutes.js
+++ routes/readerRoutes.js
@@ -1,21 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
+const { Authors } = require("../sequelize/models");
 const { Readers } = require("../sequelize/models");
+const { Events } = require("../sequelize/models");
 
-const bodyParser = require("body-parser");
-
+// MIDDLE WARE
 router.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
-
 router.use(bodyParser.json());
-
 router.use(cookieParser());
+router.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false,
+      maxAge: 2592000000,
+    },
+  })
+);
+const authenticate = (req, res, next) => {
+  if (req.session.user) {
+    next();
+  } else if (req.path == "/login") {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+};
+// MIDDLEWARE
 
 router.post("/create_reader", (req, res) => {
   const { email, password, firstName, lastName, funFact } = req.body;
@@ -37,21 +58,29 @@ router.get("/account", (req, res) => {
   res.send(`Successful login, here is your account page, ${reader.firstName}`);
 });
 
-router.post("/login_reader", async (req, res) => {
+router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const reader = await Readers.findOne({
     where: {
       email: email,
     },
   });
-  const validPassword = await bcrypt.compare(password, reader.password);
-  if (validPassword) {
-    res.render("pages/readerDash", { reader: reader });
-  } else {
-    res
-      .status(403)
-      .send("That is not a valid user. Please check email and password.");
-  }
+  bcrypt.compare(password, reader.password, (err, result) => {
+    if (err) {
+      res.send(err);
+      return;
+    }
+    if (!result) {
+      res.send("Login Credentials are invalid. Please try again.");
+    } else {
+      req.session.user = reader.dataValues;
+      res.redirect("/reader/dash");
+    }
+  });
+});
+
+router.get("/dash", authenticate, (req, res) => {
+  res.render("pages/readerDash");
 });
 
 module.exports = router;
