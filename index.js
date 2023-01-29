64bf2c27689002cc9608039dee80d660a01dba9d
jssklynn <jessikalynnadams@gmail.com>
2023-01-29 13:12:29 -0700

    created guest routes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 3027;
 const authorRoutes = require("./routes/authorRoutes");
 const readerRoutes = require("./routes/readerRoutes");
+const guestRoutes = require("./routes/guestRoutes");
 const path = require("path");
 
 // MIDDLEWARE //
@@ -10,9 +11,9 @@ app.use(express.json());
 const bodyParser = require("body-parser");
 
 app.use(
-	bodyParser.urlencoded({
-		extended: true,
-	})
+  bodyParser.urlencoded({
+    extended: true,
+  })
 );
 
 app.set("views", path.join(__dirname, "views"));
@@ -21,26 +22,27 @@ app.set("view engine", "ejs");
 
 app.use("/author", authorRoutes);
 app.use("/reader", readerRoutes);
+app.use("/guest", guestRoutes);
 // MIDDLEWARE //
 
 app.get("/", (req, res) => {
-	res.render("pages/index");
+  res.render("pages/index");
 });
 
 app.get("/login", (req, res) => {
-	res.render("pages/login");
+  res.render("pages/login");
 });
 
 app.get("/signup", (req, res) => {
-	res.render("pages/signup");
+  res.render("pages/signup");
 });
 
 app.get("/about", (req, res) => {
-	res.render("pages/about");
+  res.render("pages/about");
 });
 
 app.get("/contact", (req, res) => {
-	res.render("pages/contact");
+  res.render("pages/contact");
 });
 
 app.listen(PORT, console.log(`Listening on port ${PORT}`));
