05d61b4bf4852c8495a342d57e44612e7f0b6f5c
jssklynn <jessikalynnadams@gmail.com>
2023-01-29 16:38:22 -0700

    changed datatypes in events

---
+++ sequelize/migrations/20230129233502-changingDataTypesidFree2.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return Promise.all([
+      queryInterface.changeColumn("Events", "isFree", {
+        type: Sequelize.STRING,
+      }),
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return Promise.all([queryInterface.changeColumn("Events", "isFree")]);
+  },
+};
